7675b372099a142968e35177acea98f21ef01d58
nofunstudio <jimmy@nofun.io>
2025-01-25 21:57:30 -0700

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,11 +1,14 @@
 # __init__.py
 from .layered_infinite_zoom import LayeredInfiniteZoom
+from .replicate_API import APIGenerateReplicate
 
 NODE_CLASS_MAPPINGS = {
-    "LayeredInfiniteZoom": LayeredInfiniteZoom
+    "LayeredInfiniteZoom": LayeredInfiniteZoom,
+    "APIGenerateReplicate": APIGenerateReplicate
 }
 NODE_DISPLAY_NAME_MAPPINGS = {
-    "LayeredInfiniteZoom": "Layered Infinite Zoom"
+    "LayeredInfiniteZoom": "Layered Infinite Zoom",
+    "APIGenerateReplicate": "Replicate API"
 }
 
 __all__ = [
